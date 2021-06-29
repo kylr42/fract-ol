4b0df74b66e5a32fe7a29b00f25e80f4024e263b
Jimbo Siona <jsiona@et-n4.kzn.21-school.ru>
2021-06-29 08:12:37 +0300

    v0.1

--- handlers.c
+++ handlers.c
@@ -3,14 +3,14 @@
 int	ft_close(int code)
 {
 	if (code == 0)
-		ft_putstr_fd(RED
-			"ERROR!!!\nEx.: ./fractol [Julia / Mandelbrot / Mandelbar]\n"
+		ft_putendl_fd(RED
+			"Ex.: ./fractol [Julia / Mandelbrot / Mandelbar]"
 			RESET, 1);
 	else if (code == 1)
-		ft_putstr_fd(RED"ERROR!!!\nNot valid fractal name!\n"RESET, 1);
+		ft_putendl_fd(RED"Er.: not valid fractal name!"RESET, 1);
 	else if (code == 53)
 	{
-		ft_putstr_fd(MAG"Close window! Bye-bye!!"RESET, 1);
+		ft_putendl_fd(MAG"Close window! Bye-bye!!"RESET, 1);
 		exit(0);
 	}
 	exit(1);
